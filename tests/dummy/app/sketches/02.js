e2f62b9c95653dfea8e1676bc3abbe41cf975fee
ampatspell <ampatspell@gmail.com>
2018-04-28 00:27:42 +0300

    add basic canvas computed prop

--- tests/dummy/app/sketches/02.js
+++ tests/dummy/app/sketches/02.js
@@ -1,21 +1,22 @@
-import { computed } from '@ember/object';
-import Sketch, { tasks } from 'ember-cli-escpos/sketch';
-import { createCanvas, canvasToBlob } from 'ember-cli-escpos/canvas';
+import Sketch, { tasks, canvas } from 'ember-cli-escpos/sketch';
 import { atkinson } from 'ember-cli-escpos/canvas/pixels';
 
 export default Sketch.extend({
 
-  picture: computed(async function() {
-    let { canvas, ctx, size } = createCanvas(384, 200);
-    ctx.fillStyle = '#555';
-    ctx.fillRect(0, 0, size.width, size.height);
+  picture: canvas({
+    size: {
+      width: 384,
+      height: 200
+    },
+    draw(canvas, ctx, size) {
+      ctx.fillStyle = '#555';
+      ctx.fillRect(0, 0, size.width, size.height);
 
-    let pixels = ctx.getImageData(0, 0, size.width, size.height);
-    pixels = atkinson(pixels);
-    ctx.putImageData(pixels, 0, 0);
-
-    return canvasToBlob(canvas);
-  }).readOnly(),
+      let pixels = ctx.getImageData(0, 0, size.width, size.height);
+      pixels = atkinson(pixels);
+      ctx.putImageData(pixels, 0, 0);
+    }
+  }),
 
   tasks: tasks('picture', async function(printer) {
     printer.reset();
