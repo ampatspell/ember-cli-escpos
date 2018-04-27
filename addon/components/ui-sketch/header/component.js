fe47184423ee73af83ea08d94eeb9edae3887817
Arnis Vuskans <ampatspell@gmail.com>
2018-04-27 19:00:01 +0300

    add prev-next in header

--- addon/components/ui-sketch/header/component.js
+++ addon/components/ui-sketch/header/component.js
@@ -1,7 +1,34 @@
 import Component from '@ember/component';
 import layout from './template';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
+import { or } from '@ember/object/computed';
+
+const sketch = delta => computed('sketches.names', 'sketch', function() {
+  let id = this.sketch.id;
+  let names = this.sketches.names;
+  let idx = names.indexOf(id);
+  if(idx === -1) {
+    return;
+  }
+  let i = idx + delta;
+  if(i < 0 || i > names.length - 1) {
+    return;
+  }
+  return names[i];
+}).readOnly();
 
 export default Component.extend({
   classNameBindings: [ ':ui-sketch-header' ],
-  layout
+  layout,
+
+  sketches: service(),
+
+  next: sketch(+1),
+  prev: sketch(-1),
+
+  prevOrNext: or('prev', 'next'),
+
+  route: 'sketches.sketch'
+
 });
