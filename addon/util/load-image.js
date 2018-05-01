f3b51d19207d4daf1258a0c27cb472f84cd2d70c
ampatspell <ampatspell@gmail.com>
2018-05-01 06:28:57 +0300

    add proxy for images

--- addon/util/load-image.js
+++ addon/util/load-image.js
@@ -1,8 +1,15 @@
 import { Promise } from 'rsvp';
 
-export default url => new Promise((resolve, reject) => {
+const fullURL = (url, proxy) => {
+  if(proxy) {
+    return `/escpos/proxy?url=${encodeURIComponent(url)}`;
+  }
+  return url;
+};
+
+export default (url, proxy=false) => new Promise((resolve, reject) => {
   let image = new Image();
   image.onload = () => resolve(image);
   image.onerror = () => reject(new Error('Image load failed'));
-  image.src = url;
+  image.src = fullURL(url, proxy);
 });
